31666c936be1d53f6511dfb1397852eac0eb48a0
Rachelle Lee <rachedeveloper@gmail.com>
2021-06-14 21:57:15 -0700

    basic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,149 @@
-import logo from './logo.svg';
+import {
+  createMuiTheme,
+  ThemeProvider,
+  makeStyles,
+} from '@material-ui/core/styles';
+import { Typography } from '@material-ui/core';
+import NavBar from './components/NavBar';
+import Grid from './components/Grid';
+import Footer from './components/Footer';
 import './App.css';
 
+import LocalDiningIcon from '@material-ui/icons/LocalDining';
+import LocalHotelIcon from '@material-ui/icons/LocalHotel';
+import LocalGasStationIcon from '@material-ui/icons/LocalGasStation';
+import LocalGroceryStoreIcon from '@material-ui/icons/LocalGroceryStore';
+import LocalMallIcon from '@material-ui/icons/LocalMall';
+import LocalAtmIcon from '@material-ui/icons/LocalAtm';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#2e1667',
+    },
+    secondary: {
+      main: '#c7d8ed',
+    },
+  },
+  typography: {
+    fontFamily: ['Roboto'],
+    h4: {
+      fontWeight: 600,
+      fontSize: 28,
+      lineHeight: '2rem',
+    },
+    h5: {
+      fontWeight: 100,
+      lineHeight: '2rem',
+    },
+  },
+});
+
+const styles = makeStyles({
+  wrapper: {
+    width: '65%',
+    margin: 'auto',
+    textAlign: 'center',
+  },
+  bigSpace: {
+    marginTop: '5rem',
+  },
+  littleSpace: {
+    marginTop: '2.5rem',
+  },
+  grid: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+  },
+});
+
 function App() {
+  const classes = styles();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeProvider theme={theme}>
+        <NavBar />
+        <div className={classes.wrapper}>
+          <Typography variant="h4" className={classes.bigSpace} color="primary">
+            We are WASHINGTON STATE !
+          </Typography>
+          <Typography
+            variant="h5"
+            className={classes.littleSpace}
+            color="primary"
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
+            sodales congue tristique. Cras non pretium sem. Duis interdum lorem
+            sit amet ligula pretium, sed rutrum urna semper. Pellentesque
+            habitant morbi tristique senectus et netus et malesuada fames ac
+            turpis egestas. Phasellus egestas gravida ullamcorper.
+          </Typography>
+        </div>
+        <div className={`${classes.grid} ${classes.bigSpace}`}>
+          <Grid
+            icon={
+              <LocalDiningIcon
+                style={{ fill: '#4360A6', height: '125', width: '125' }}
+              />
+            }
+            title="Restaurants"
+            btnTitle="Show me More"
+          />
+          <Grid
+            icon={
+              <LocalHotelIcon
+                style={{ fill: '#449A76', height: '125', width: '125' }}
+              />
+            }
+            title="Hotels"
+            btnTitle="Show me More"
+          />
+          <Grid
+            icon={
+              <LocalGasStationIcon
+                style={{ fill: '#D05B2D', height: '125', width: '125' }}
+              />
+            }
+            title="Gas Stations"
+            btnTitle="Show me More"
+          />
+        </div>
+        <div className={`${classes.grid} ${classes.littleSpace}`}>
+          <Grid
+            icon={
+              <LocalGroceryStoreIcon
+                style={{ fill: '#5EA780', height: '125', width: '125' }}
+              />
+            }
+            title="Grocery Stores"
+            btnTitle="Show me More"
+          />
+          <Grid
+            icon={
+              <LocalMallIcon
+                style={{ fill: '#E69426', height: '125', width: '125' }}
+              />
+            }
+            title="Malls"
+            btnTitle="Show me More"
+          />
+          <Grid
+            icon={
+              <LocalAtmIcon
+                style={{ fill: '#2EA09D', height: '125', width: '125' }}
+              />
+            }
+            title="ATM/Banks"
+            btnTitle="Show me More"
+          />
+        </div>
+        <div className={classes.bigSpace}>
+          <Footer />
+        </div>
+      </ThemeProvider>
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import CustomBtn from './CustomBtn'
+import logo from '../WA.png'
+import logoMobile from '../WAMobile.png'
+import {Toolbar, Typography} from '@material-ui/core'
+import {makeStyles} from "@material-ui/core/styles";
+
+const styles = makeStyles({
+  bar: {
+    paddingTop: '1.15rem',
+    backgroundColor: '#fff',
+    ['@media (max-width:780px)']: {
+      flexDirection: 'column',
+    },
+  },
+  logo: {
+    width: '15%',
+    ['@media (max-width:780px)']: {
+      display: 'none',
+    },
+  },
+  logoMobile: {
+    width: '100%',
+    display: 'none',
+    ['@media (max-width:780px)']: {
+      display: 'inline-block',
+    },
+  },
+  menuItem: {
+    cursor: 'pointer',
+    flexGrow: 1,
+    '&:hover': {
+      color: '#4f25c8',
+    },
+    ['@media (max-width:780px)']: {
+      paddingBottom: '1rem',
+    },
+  },
+});
+
+function NavBar() {
+  const classes = styles();
+  return (
+    <Toolbar
+      position="sticky"
+      color="rgba(0, 0, 0, 0.87)"
+      className={classes.bar}
+    >
+      <img src={logo} className={classes.logo} />
+      <img src={logoMobile} className={classes.logoMobile} />
+      <Typography variant="h6" className={classes.menuItem}>
+        About
+      </Typography>
+      <Typography variant="h6" className={classes.menuItem}>
+        Counties
+      </Typography>
+      <Typography variant="h6" className={classes.menuItem}>
+        Top Attractions
+      </Typography>
+      <Typography variant="h6" className={classes.menuItem}>
+        Events
+      </Typography>
+      <Typography variant="h6" className={classes.menuItem}>
+        Contact Us
+      </Typography>
+      <CustomBtn txt="View Map" />
+    </Toolbar>
+  );
+}
+
+
+export default NavBar
